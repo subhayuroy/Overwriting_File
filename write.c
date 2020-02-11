b107b9fbf777573804c2bc49d5abd2663c50a5c9
Subhayu Roy <55306904+subhayuroy@users.noreply.github.com>
2020-02-11 23:16:01 +0530

    Creating file

---
+++ write.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<unistd.h>
+#include<fcntl.h>
+#include<stdlib.h>
+#include<string.h>
+#include<sys/stat.h>
+int main(int arg, char *argv[])
+{
+	int fd, rc;
+	char *buffer= "Testing a write: ";
+	fd= open("./afile", O_RDWR | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR);
+	rc= write(fd, buffer, strlen(buffer));
+	printf("wrote %d bytes\n",rc);
+	close(fd);
+	exit(EXIT_SUCCESS);
+}
