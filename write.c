06f46251bd396fa892fcae2e3379cf38fd722143
Subhayu Roy <55306904+subhayuroy@users.noreply.github.com>
2020-02-16 13:23:39 -0500

    MInor changes

--- write.c
+++ write.c
@@ -8,7 +8,7 @@ int main(int arg, char *argv[])
 {
 	int fd, rc;
 	char *buffer= "Testing a write: ";
-	fd= open("./afile", O_RDWR | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR);
+	fd= open("./afile", O_RDWR | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR)
 	rc= write(fd, buffer, strlen(buffer));
 	printf("wrote %d bytes\n",rc);
 	close(fd);
